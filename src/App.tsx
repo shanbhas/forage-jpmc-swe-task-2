a74c7548e70d3a9a81ea1fbfe89c1d06cb948d91
SaiBhags <shanbhagsaish@gmail.com>
2023-07-08 08:38:49 +0400

    app-modification2

--- src/App.tsx
+++ src/App.tsx
@@ -41,11 +41,21 @@ class App extends Component<{}, IState> {
    * Get new data from server and update the state with the new data
    */
   getDataFromServer() {
+    let x =0 ;
+    const interval = setInterval(()=> {
     DataStreamer.getData((serverResponds: ServerRespond[]) => {
+      this.setState({
+        data:serverResponds,
+        showGraph: true,
+      });
+    });
       // Update the state by creating a new array of data that consists of
       // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
-    });
+      x++;
+      if(x>1000){
+        clearInterval(interval);
+      }
+    } , 100);
   }
 
   /**
